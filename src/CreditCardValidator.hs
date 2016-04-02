82c722dd467605c3e109ab1265878b243099eda9
Ignat Insarov <kindaro@gmail.com>
2016-04-02 13:11:43 +0300

    Improve on readability (Surely that's an improvement my dear?).

--- src/CreditCardValidator.hs
+++ src/CreditCardValidator.hs
@@ -1,21 +1,28 @@
 module CreditCardValidator where
 
 import Control.Monad
-import Control.Applicative
 
 import Helpers
 
 main :: IO ()
-main = do
-    liftM (luhnDigit . read . filterDigits) getLine >>= print
+main = liftM (luhnDigit . read . filterDigits) getLine >>= print
     where
-        filterDigits = filter  (`elem` ['0'..'9'])
+        filterDigits = filter (`elem` digits)
+        digits = ['0'..'9']
 
 luhnDigit :: Integer -> Integer
-luhnDigit n = mod10 . sum . concat . (map numberToDigits) . (map doubleRights) $ eiEven $ reverse $ numberToDigits n
+luhnDigit = mod10
+          . sum
+          . concat
+          . (map numberToDigits)
+          . (map doubleRights)
+          . eiEven
+          . reverse
+          . numberToDigits
     where
         mod10 = flip mod 10
         doubleRights = either id (*2)
-        eiEven = (map (either (Left . snd) (Right . snd))) . (eiFilter fst) . zip (even <$> [1..])
+        eiEven = (eiMap snd snd) . (eiFilter fst) . zip (map even [1..])
 
+luhnCheck :: Integer -> Bool
 luhnCheck = (0 ==) . luhnDigit
--- src/Helpers.hs
+++ src/Helpers.hs
@@ -1,4 +1,4 @@
-module Helpers (eiFilter, numberToDigits) where
+module Helpers (eiFilter, eiMap, numberToDigits) where
 
 eiFilter :: (a -> Bool) -> [a] -> [Either a a]
 eiFilter f xs = map (conditional f) xs
@@ -6,6 +6,13 @@ eiFilter f xs = map (conditional f) xs
     conditional f x
         | f x = Right x
         | otherwise = Left x
+
+eiMap :: (a -> c) -> (b -> d) -> [Either a b] -> [Either c d]
+eiMap _ _ [] = []
+eiMap fL fR (x:xs) = f x : eiMap fL fR xs
+    where
+    f (Left x) = Left $ fL x
+    f (Right y) = Right $ fR y
 
 numberToDigits :: Integer -> [Integer]
 numberToDigits = (map (read . return)) . show
